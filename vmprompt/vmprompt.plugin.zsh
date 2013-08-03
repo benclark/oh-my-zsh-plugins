9e268d375a656160858fd00153098e4f4335d024
Benjamin Clark <ben@benclark.com>
2013-08-03 10:33:54 -0400

    Updating vmprompt.plugin.zsh to support both VirtualBox and Parallels. Renaming get_runningvms() to vbox_prompt().

--- vmprompt/vmprompt.plugin.zsh
+++ vmprompt/vmprompt.plugin.zsh
@@ -1,5 +1,11 @@
-get_runningvms() {
+vbox_prompt() {
   /usr/bin/VBoxManage list runningvms | egrep -o '^"[^"]+"' 2>/dev/null | sed -e 's|"||g' | while read vms; do
+    echo -n '•'
+  done
+}
+
+parallels_prompt() {
+  /usr/bin/prlctl list --no-header -o name 2>/dev/null | while read vms; do
     echo -n '•'
   done
 }
