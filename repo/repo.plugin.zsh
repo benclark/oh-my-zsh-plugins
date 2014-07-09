1d4985f87695ab42e2efb43869af5799cca69c73
Benjamin Clark <ben@benclark.com>
2014-07-09 15:43:08 -0400

    Adding support for chef-repos and chef-cookbooks.

--- repo/repo.plugin.zsh
+++ repo/repo.plugin.zsh
@@ -1,6 +1,7 @@
 _repo_list() {
-  cd $HOME/Sites
-  _wanted 'repo' expl 'repo' compadd $(ls -1d *)
+  _wanted 'repo' expl 'repo' compadd $(find "$HOME/Sites" -maxdepth 1 -mindepth 1 -type d -exec basename {} \+)
+  _wanted 'repo' expl 'repo' compadd $(find "$HOME/Sites/chef-repos" -maxdepth 1 -mindepth 1 -type d -exec basename {} \+)
+  _wanted 'repo' expl 'repo' compadd $(find "$HOME/Sites/chef-cookbooks" -maxdepth 1 -mindepth 1 -type d -exec basename {} \+)
 }
 
 repo() {
@@ -9,8 +10,10 @@ repo() {
     cd "$basedir/repo/docroot"
   elif [ -d "$basedir/repo" ]; then
     cd "$basedir/repo"
-  elif [ -d "$basedir/trunk" ]; then
-    cd "$basedir/trunk"
+  elif [ -d "$HOME/Sites/chef-repos/$1" ]; then
+    cd "$HOME/Sites/chef-repos/$1"
+  elif [ -d "$HOME/Sites/chef-cookbooks/$1" ]; then
+    cd "$HOME/Sites/chef-cookbooks/$1"
   elif [ -d "$basedir" ]; then
     cd "$basedir"
   else
